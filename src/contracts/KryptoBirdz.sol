07abe7e114a7a0cf80bf0092988d7a3f9185e8d4
Daniel Morales <danieldev0612@gmail.com>
2022-06-14 13:16:21 -0700

    update migration, create KryptoBirdz smart contract

---
+++ src/contracts/KryptoBirdz.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract KryptoBirdz {
+    string public name;
+    string public symbol;
+
+    constructor() {
+        name = "Kryptobird";
+        symbol = "KBIRDZ";
+    }
+}
