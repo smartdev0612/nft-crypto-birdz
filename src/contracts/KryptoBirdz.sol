71d3fa64984b0c950c582f6a4995845bf7c24e6d
Daniel Morales <danieldev0612@gmail.com>
2022-06-15 18:44:49 -0700

    add mint function in KryptoBirdz.sol

--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -4,5 +4,24 @@ pragma solidity >=0.4.22 <0.9.0;
 import "./ERC721Connector.sol";
 
 contract KryptoBirdz is ERC721Connector {
+    // array to store our nfts
+    string[] public kryptoBirdz;
+
+    mapping(string => bool) _kryptoBirdzExists;
+
     constructor() ERC721Connector("KryptoBird", "KBIRDZ") {}
+
+    function mint(string memory _kryptoBird) public {
+        require(
+            !_kryptoBirdzExists[_kryptoBird],
+            "Error - kryptoBird already exists"
+        );
+
+        kryptoBirdz.push(_kryptoBird);
+        uint256 _id = kryptoBirdz.length - 1;
+
+        _mint(msg.sender, _id);
+
+        _kryptoBirdzExists[_kryptoBird] = true;
+    }
 }
