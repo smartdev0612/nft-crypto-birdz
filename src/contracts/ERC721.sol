f7f74b8c742fb6cdb58ead489289c1d17b0895f3
Daniel Morales <danieldev0612@gmail.com>
2022-06-16 11:42:08 -0700

    update solidity version

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -1,18 +1,39 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.0;
 
 contract ERC721 {
+    // Mapping from token id to the owner
+    mapping(uint256 => address) private _tokenOwner;
+
+    // Mapping from owner to number of owned tokens
+    mapping(address => uint256) private _ownedTokensCount;
+
     event Transfer(
         address indexed from,
         address indexed to,
         uint256 indexed tokenId
     );
 
-    // Mapping from token id to the owner
-    mapping(uint256 => address) private _tokenOwner;
+    /// @notice Count all NFTs assigned to an owner
+    /// @dev NFTs assigned to the zero address are considered invalid, and this
+    ///  function throws for queries about the zero address.
+    /// @param _owner An address for whom to query the balance
+    /// @return The number of NFTs owned by `_owner`, possibly zero
+    function balanceOf(address _owner) external view returns (uint256) {
+        require(_owner != address(0), "owner query for non-existent token");
+        return _ownedTokensCount[_owner];
+    }
 
-    // Mapping from owner to number of owned tokens
-    mapping(address => uint256) private _ownedTokensCount;
+    /// @notice Find the owner of an NFT
+    /// @dev NFTs assigned to zero address are considered invalid, and queries
+    ///  about them do throw.
+    /// @param _tokenId The identifier for an NFT
+    /// @return The address of the owner of the NFT
+    function ownerOf(uint256 _tokenId) external view returns (address) {
+        address owner = _tokenOwner[_tokenId];
+        require(owner != address(0), "owner query for non-existent token");
+        return owner;
+    }
 
     function _exists(uint256 tokenId) internal view returns (bool) {
         address owner = _tokenOwner[tokenId];
@@ -20,7 +41,7 @@ contract ERC721 {
         return owner != address(0);
     }
 
-    function _mint(address to, uint256 tokenId) internal {
+    function _mint(address to, uint256 tokenId) internal virtual {
         // requires that the address isn't zero
         require(to != address(0), "ERC721: minting to the zero address!");
         // requires that the token doesn't already exist
