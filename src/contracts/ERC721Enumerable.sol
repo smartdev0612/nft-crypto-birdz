9b3fe8a8003010871205296c4132255f4fdd9118
Daniel Morales <danieldev0612@gmail.com>
2022-06-17 23:37:47 -0700

    add token enumeration functions

--- src/contracts/ERC721Enumerable.sol
+++ src/contracts/ERC721Enumerable.sol
@@ -6,6 +6,15 @@ import "./ERC721.sol";
 contract ERC721Enumerable is ERC721 {
     uint256[] private _allTokens;
 
+    // mapping from tokenId to position in _allTokens array
+    mapping(uint256 => uint256) private _allTokensIndex;
+
+    // mapping of owner to list of all owner token ids
+    mapping(address => uint256[]) private _ownedTokens;
+
+    // mapping from token ID to index of the owner tokens list
+    mapping(uint256 => uint256) private _ownedTokensIndex;
+
     /// @notice Count NFTs tracked by this contract
     /// @return A count of valid NFTs tracked by this contract, where each one of
     ///  them has an assigned and queryable owner not equal to the zero address
@@ -13,24 +22,19 @@ contract ERC721Enumerable is ERC721 {
         return _allTokens.length;
     }
 
-    /// @notice Enumerate valid NFTs
-    /// @dev Throws if `_index` >= `totalSupply()`.
-    /// @param _index A counter less than `totalSupply()`
-    /// @return The token identifier for the `_index`th NFT,
-    ///  (sort order not specified)
-    function tokenByIndex(uint256 _index) external view returns (uint256) {}
+    function _mint(address to, uint256 tokenId) internal override(ERC721) {
+        super._mint(to, tokenId);
+        _addTokensToAllTokenEnumeration(tokenId);
+        _addTokensToOwnerEnumeration(to, tokenId);
+    }
 
-    /// @notice Enumerate NFTs assigned to an owner
-    /// @dev Throws if `_index` >= `balanceOf(_owner)` or if
-    ///  `_owner` is the zero address, representing invalid NFTs.
-    /// @param _owner An address where we are interested in NFTs owned by them
-    /// @param _index A counter less than `balanceOf(_owner)`
-    /// @return The token identifier for the `_index`th NFT assigned to `_owner`,
-    ///   (sort order not specified)
-    function tokenOfOwnerByIndex(address _owner, uint256 _index)
-        external
-        view
-        returns (uint256);
+    function _addTokensToAllTokenEnumeration(uint256 tokenId) private {
+        _allTokensIndex[tokenId] = _allTokens.length;
+        _allTokens.push(tokenId);
+    }
 
-    function _mint(address to, uint256 tokenId) override(ERC721) {}
+    function _addTokensToOwnerEnumeration(address to, uint256 tokenId) private {
+        _ownedTokensIndex[tokenId] = _ownedTokens[to].length;
+        _ownedTokens[to].push(tokenId);
+    }
 }
